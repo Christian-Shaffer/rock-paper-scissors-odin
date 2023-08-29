dd7cc43ef636834d93a6c89e1124bd16a5e4135c
Christian Shaffer <calexandershaffer@gmail.com>
2023-08-29 19:19:47 -0400

    Add game logic
    Just added all the game logic. One bug that I still need to resolve
    is incorrect spelling still counting toward the loop sequency.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rock-paper-scissors-odin
+Rock Paper Scissors is a small project that The Odin Project has students complete. It's just a basic command line program where we play rock paper scissors vs the computer. Later it will be expanded to have a GUI. 
+
+Link to The Odin Project's page on this assignment: https://www.theodinproject.com/lessons/foundations-rock-paper-scissors
---
+++ scripts.js
@@ -0,0 +1,56 @@
+const moves = ['Rock', 'Paper', 'Scissors'];
+let playerWinCount = 0;
+let computerWinCount = 0;
+let tieCount = 0;
+
+function getComputerChoice () {
+    const choice = moves[Math.floor(Math.random() * 3)];
+    return choice;
+}
+
+function playRound (playerSelection = prompt('Pick'), computerSelection = getComputerChoice()) {
+    const formattedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.toLowerCase().slice(1);
+
+    if (moves.includes(formattedPlayerSelection) == false) {
+        console.log('Spell your choice correctly! :-)');
+    } else if (formattedPlayerSelection == 'Rock' && computerSelection == 'Paper') {
+        console.log('computer win');
+        return ++computerWinCount; 
+    } else if (formattedPlayerSelection == 'Scissors' && computerSelection == 'Paper') {
+        console.log('player win');
+        return ++playerWinCount;
+    } else if (formattedPlayerSelection == 'Scissors' && computerSelection == 'Rock') {
+        console.log('computer win');
+        return ++computerWinCount;
+    } else if (formattedPlayerSelection == 'Paper' && computerSelection == 'Rock') {
+        console.log('player win');
+        return ++playerWinCount;
+    } else if (formattedPlayerSelection == 'Rock' && computerSelection == 'Scissors') {
+        console.log('player win');
+        return ++playerWinCount;
+    } else if (formattedPlayerSelection == 'Paper' && computerSelection == 'Scissors') {
+        console.log('computer win');
+        return ++computerWinCount;
+    } else {
+        console.log("It's a tie");
+        return ++tieCount;
+    }
+}
+
+function game () {
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (playerWinCount > computerWinCount) { 
+        let playerWon = true;
+        console.log(`You won more! Your wins: ${playerWinCount}. Computer wins: ${computerWinCount}.`);
+    } else {
+        let computerWon = true;
+        console.log(`The blasted computer won! Your wins: ${playerWinCount}. Computer wins: ${computerWinCount}. Ties: ${tieCount}.`);
+    }
+}
+
+
+game();
+
+// Exit properly on wrong spelling fix needed
